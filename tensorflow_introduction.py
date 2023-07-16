aaa14db1891c3353983bdef520e13760e86df37a
bobobulut <borayazgan2000@gmail.com>
2023-07-16 21:34:54 +0300

    Update tensorflow_introduction.py

--- tensorflow_introduction.py
+++ tensorflow_introduction.py
@@ -104,7 +104,7 @@ The example below shows how to change the shape of a tensor.
 tensor1 = tf.ones([1,2,3])  # tf.ones() creates a shape [1,2,3] tensor full of ones
 tensor2 = tf.reshape(tensor1, [2,3,1])  # reshape existing data to shape [2,3,1]
 tensor3 = tf.reshape(tensor2, [3, -1])  # -1 tells the tensor to calculate the size of the dimension in that place
-                                        # this will reshape the tensor to [3,3]
+                                        # this will reshape the tensor to [3,2]
 
 # The numer of elements in the reshaped tensor MUST match the number in the original
 
